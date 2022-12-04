07f6bc0f52ad6e4dfaf96678d05e16b974553dbd
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2022-12-05 00:31:20 +0300

    Refactor: Better visualization for status counts

--- testprocess/core/slackbot.py
+++ testprocess/core/slackbot.py
@@ -16,21 +16,18 @@ class SlackBot:
     def send_results(self, test_result: dict) -> None:
         """This function sends a message to the channel."""
         json_logs = json.dumps(test_result.get("logs"), indent=4)
-        json_status_counts = json.dumps(test_result.get("status_counts"), indent=4)
 
         message: str = (
             f'==== Test Results for _{test_result.get("test_name")}_ ====\n'
             f'- Status: *{test_result.get("status_of_test")}*\n'
+            f'\t- Status.SUCCESS: {test_result.get("status_counts").get("Status.SUCCESS")}\n'
+            f'\t- Status.ERROR: {test_result.get("status_counts").get("Status.ERROR")}\n'
+            f'\t- Status.TIMEOUT: {test_result.get("status_counts").get("Status.TIMEOUT")}\n'
             f'- Device Port: {test_result.get("device_port")}\n'
             f'- Total Elapsed Time: {test_result.get("total_elapsed_time")}\n\n'
             f'```\n'
             f'{json_logs}\n'
             f'```\n'
-            f'- Number of Status Types:\n'
-            f'```\n'
-            f'{json_status_counts}\n'
-            f'```\n'
-            f'==== End of Test Results ===='
         )
         try:
             while True:
